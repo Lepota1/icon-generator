8b12767cba5d528a0fe55656f9d90ff6486f8ed8
Yauheni Lapets <lapezbyby@gmail.com>
2023-05-18 14:59:17 +0200

    addid generat trcp endpoint

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,14 +1,12 @@
 import { createTRPCRouter } from "~/server/api/trpc";
 import { exampleRouter } from "~/server/api/routers/example";
+import { generateRouter } from "~/server/api/routers/generate";
 
 /**
  * This is the primary router for your server.
- *
- * All routers added in /api/routers should be manually added here.
+@@ -8,6 +9,7 @@ import { exampleRouter } from "~/server/api/routers/example";
  */
 export const appRouter = createTRPCRouter({
   example: exampleRouter,
+  generate: generateRouter,
 });
-
-// export type definition of API
-export type AppRouter = typeof appRouter;
